63e41624a575e9fa00fb4339cbc2313516454f86
Anushka Chouhan <anushkachouhan064@gmail.com>
2024-06-24 23:06:47 +0530

    Commit changes

---
+++ Array&String/Q1_MergeStringsAlternately.java
@@ -0,0 +1,25 @@
+public class Q1_MergeStringsAlternately{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String word1 = "abc", word2 = "pqr";
+        System.out.println(mergeAlternately(word1, word2));
+    }
+    public static String mergeAlternately(String word1, String word2) {
+        int n;
+        String ans ="";
+        if(word1.length() > word2.length()){
+            n = word1.length();
+        }else{
+            n = word2.length();
+        }
+        for(int i = 0; i< n; i++){
+            if(word1.length() > i){
+            ans += word1.charAt(i);
+            }
+            if(word2.length() > i){
+            ans+= word2.charAt(i);
+            }
+        }
+        return ans;
+    }
+}
